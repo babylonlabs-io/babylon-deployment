3931166455a5f40b299d9fb1735c75647b30067c
Fangyu Gai <greferry@gmail.com>
2024-09-04 23:07:59 +0800

    add fpd sending pub rand

---
+++ deployments/local/bbn-start-and-upgrade-pubrand-commit.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -eu
+
+# USAGE:
+# ./bbn-start-and-upgrade-signet-launch.sh
+
+# Runs the signet launch upgrade '-'
+# It adds new btc headers to the running chain
+
+CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
+
+NODE_BIN="${1:-$CWD/../../babylon/build/babylond}"
+
+STARTERS="${STARTERS:-$CWD/starters}"
+UPGRADES="${UPGRADES:-$CWD/upgrades}"
+
+CHAIN_DIR="${CHAIN_DIR:-$CWD/data}"
+DATA_OUTPUTS="${DATA_OUTPUTS:-$CHAIN_DIR/outputs}"
+BTC_BASE_HEADER_FILE="${BTC_BASE_HEADER_FILE:-$DATA_OUTPUTS/btc-base-header.json}"
+
+. $CWD/helpers.sh
+mkdir -p $DATA_OUTPUTS
+
+# Start bitcoind
+$STARTERS/start-bitcoind.sh
+sleep 2
+
+# writes block zero from BTC to babylon
+writeBaseBtcHeaderFile $BTC_BASE_HEADER_FILE
+
+# Setup and start single node with base btc header set
+BTC_BASE_HEADER_FILE=$BTC_BASE_HEADER_FILE $STARTERS/start-babylond-single-node.sh
+
+# wait for a block
+sleep 6
+
+# Writes the btc headers into babylond upgrades
+$STARTERS/setup-staking-indexer.sh
+
+btcHeaderTipBeforeUpgrade=$($NODE_BIN q btclightclient tip -o json | jq .header.height -r)
+fpsLengthBeforeUpgrade=$($NODE_BIN q btcstaking finality-providers --output json | jq '.finality_providers | length')
+
+# Gov prop, waits for block, kill and reestart in the new version
+PRE_BUILD_UPGRADE_SCRIPT=$UPGRADES/write-upgrades-data.sh SOFTWARE_UPGRADE_FILE=$UPGRADES/props/signet-launch.json \
+  BABYLON_VERSION_WITH_UPGRADE="gai/fix-conflicts" $UPGRADES/upgrade-single-node.sh
+
+# checks if all the btc headers and fps were added '-'
+upgradeHeight=$($NODE_BIN q upgrade applied signet-launch --output json | jq ".height" -r)
+btcHeaderTipAfterUpgrade=$($NODE_BIN q btclightclient tip -o json | jq .header.height -r)
+fpsLengthAfterUpgrade=$($NODE_BIN q btcstaking finality-providers --output json | jq '.finality_providers | length')
+
+if ! [[ $btcHeaderTipAfterUpgrade -gt $btcHeaderTipBeforeUpgrade ]]; then
+  echo "Upgrade should have applied a bunch of btc headers"
+  exit 1
+fi
+if ! [[ $fpsLengthAfterUpgrade -gt $fpsLengthBeforeUpgrade ]]; then
+  echo "Upgrade should have applied a bunch of finality providers"
+  exit 1
+fi
+
+echo "Signet launch upgrade was correctly executed at block height " $upgradeHeight
+echo "the last btc header height is" $btcHeaderTipAfterUpgrade
+echo "the number of finality providers increased from" $fpsLengthBeforeUpgrade " to " $fpsLengthAfterUpgrade
+
+# Start EOTS
+CHAIN_DIR=$CHAIN_DIR $STARTERS/start-eots.sh
+
+# sleeps here, because covd and fpd need funds and execute an tx bank send from user
+# to avoid acc sequence errors, just wait until produces a block.
+sleep 2
+
+# Start FPD
+CHAIN_DIR=$CHAIN_DIR $STARTERS/start-fpd.sh
+
+sleep 1000 # waits for fdp to send some txs
